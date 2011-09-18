e1f97131701bf84ad282913d2028285dfa409b9e
Sandeep Ruhil <sandeep.kumar.ruhil@gmail.com>
2011-09-18 15:50:03 +0530

    Implemented Map for the slider in index page.

--- index.php
+++ index.php
@@ -1,22 +1,20 @@
 <?php
-   include "header.php";
-   include "admin/config.php";
+include "header.php";
+include "admin/config.php";
 ?>
 <div id="slide">
   <div id="example">
 	<div id="slides">
+	  <map id="Map" name="Map">
+		<area href="about.php" coords="720,0,958,407" shape="rect">
+	  </map>
 	  <div class="slides_container" height="305px">
-		<img src="images/slider/1_03.jpg" alt="Slide 1" class="slider_image"/>
-		<img src="images/slider/2_03.jpg" alt="Slide 2" class="slider_image"/>
-		<img src="images/slider/3_03.jpg" alt="Slide 3" class="slider_image"/>
-		<img src="images/slider/4_03.jpg" alt="Slide 4" class="slider_image"/>
-		<img src="images/slider/5_03.jpg" alt="Slide 5" class="slider_image"/>
-		<img src="images/slider/6_03.jpg" alt="Slide 6" class="slider_image"/>
-		<!--	
-		   <map id="Map" name="Map">
-			 <area href="about.php" coords="645,10,951,407" shape="rect">
-		   </map>
-		   -->
+		<img src="images/slider/1_03.jpg" alt="Slide 1" class="slider_image" usemap="#Map"/>
+		<img src="images/slider/2_03.jpg" alt="Slide 2" class="slider_image" usemap="#Map"/>
+		<img src="images/slider/3_03.jpg" alt="Slide 3" class="slider_image" usemap="#Map"/>
+		<img src="images/slider/4_03.jpg" alt="Slide 4" class="slider_image" usemap="#Map"/>
+		<img src="images/slider/5_03.jpg" alt="Slide 5" class="slider_image" usemap="#Map"/>
+		<img src="images/slider/6_03.jpg" alt="Slide 6" class="slider_image" usemap="#Map"/>
 	  </div>
 	</div>
   </div>
