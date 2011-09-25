8e67d436e0c8cf3f2057cdb213d71c07e12bab26
Sandeep Ruhil <sandeep.kumar.ruhil@gmail.com>
2011-09-25 12:51:26 +0530

    Pick the impacts randomly.

--- admin/edit_impact.php
+++ admin/edit_impact.php
@@ -1,56 +1,48 @@
 <?php
-require_once('auth.php');
+  require_once('auth.php');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>SS Admin | Impact</title>
-<link href="loginmodule.css" rel="stylesheet" type="text/css" />
-</head>
-<body>
-<h1> Impact Admin </h1>
-
+  <head>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<title>SS Admin | Impact</title>
+	<link href="loginmodule.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+	<h1> Impact Admin </h1>
 <?php
-	require_once('config.php');
-    $d=$new_heading=$id=null;
-    if(array_key_exists("d",$_GET))
-	{
-		$d = mysql_real_escape_string($_GET["d"]); // if 1 delete entry with id  e = id
-    }
-if(array_key_exists("pic_desc",$_POST))
-	{
-		$new_heading = mysql_real_escape_string($_POST["pic_desc"]);
+  require_once('config.php');
+  $d=$new_heading=$id=null;
+  if(array_key_exists("d",$_GET)) {
+	$d = mysql_real_escape_string($_GET["d"]); // if 1 delete entry with id  e = id
+  }
+  if(array_key_exists("pic_desc",$_POST)) {
+	$new_heading = mysql_real_escape_string($_POST["pic_desc"]);
+  }
+  if(array_key_exists("e",$_GET)) {
+	$id = $_GET["e"]; // if this is null act as add give empty form 
+  }
+  if($d == 1 && $id != null) {
+	$q = "DELETE FROM impact WHERE id=$id";
+	$r = mysql_query($q);
+	if($r) {
+	  echo "Deleted impact with id = ".$id.'  ';
+	  echo '<a href="impact.php">Back to Impact</a>';
 	}
-if(array_key_exists("e",$_GET))
-	{
-		$id = $_GET["e"]; // if this is null act as add give empty form 
+	else {
+	  echo "Could not delete impact.";
 	}
-	if($d == 1 && $id != null)
-	{
-		$q = "DELETE FROM impact WHERE id=$id";
-		$r = mysql_query($q);
-		if($r)
-		{
-			echo "Deleted impact with id = ".$id.'  ';
-			echo '<a href="impact.php">Back to Impact</a>';
-		}
-		else
-		{
-			echo "Could not delete impact.";
-		}
-	}
-	else if($new_heading == NULL)
-	{
-		$q = "SELECT * FROM impact WHERE id=$id";
-		$r = mysql_query($q);
-		if ( $r !== false && mysql_num_rows($r) > 0 ) {
-			$a = mysql_fetch_assoc($r);
-	        $pic_desc = stripslashes($a['pic_desc']);
-	        $small_desc = stripslashes($a['small_desc']);
-    	    $description = stripslashes($a['description']);
-			$timestamp = stripslashes($a['tm']);
-	        $entry_display .= <<<ENTRY_DISPLAY
+  }
+  else if($new_heading == NULL) {
+	$q = "SELECT * FROM impact WHERE id=$id";
+	$r = mysql_query($q);
+	if ( $r !== false && mysql_num_rows($r) > 0 ) {
+	  $a = mysql_fetch_assoc($r);
+	  $pic_desc = stripslashes($a['pic_desc']);
+	  $small_desc = stripslashes($a['small_desc']);
+	  $description = stripslashes($a['description']);
+	  $timestamp = stripslashes($a['tm']);
+	  $entry_display .= <<<ENTRY_DISPLAY
 	<form action="edit_impact.php?e=$id" method="post" enctype="multipart/form-data">	
 	<p><label for="id_pic_desc">Small Image Caption:</label> <input id="id_pic_desc" type="text" name="pic_desc" value="$pic_desc" /></p>
 	<p><label for="id_small_desc">Short Description:</label> <input id="id_small_desc" type="text" name="small_desc" value="$small_desc" /></p>
@@ -59,11 +51,10 @@ if(array_key_exists("e",$_GET))
 	<p><label for="id_largeimg">Large Img File:</label> <input type="file" name="largeimg" id="id_largeimg" /></p>
 	<input type="submit" value="Upload" />
 	</form>
-
 ENTRY_DISPLAY;
-		}
-		else {
-    	 $entry_display = <<<ENTRY_DISPLAY
+	}
+	else {
+	  $entry_display = <<<ENTRY_DISPLAY
 <form action="edit_impact.php" method="post" enctype="multipart/form-data">	
 	<p><label for="id_pic_desc">Small Image Caption:</label> <input id="id_pic_desc" type="text" name="pic_desc" /></p>
 	<p><label for="id_small_desc">Short Description:</label> <input id="id_small_desc" type="text" name="small_desc" /></p>
@@ -73,101 +64,86 @@ ENTRY_DISPLAY;
 	<input type="submit" value="Upload" />
 	</form>
 ENTRY_DISPLAY;
-		}
 	}
-	else {
-		$pic_desc = stripslashes($_POST['pic_desc']);
-	    $small_desc = stripslashes($_POST['small_desc']);
-    	$description = nl2br(stripslashes($_POST['description']));
-		if ((($_FILES["smallimg"]["type"] == "image/gif")
-		|| ($_FILES["smallimg"]["type"] == "image/jpeg")
-		|| ($_FILES["smallimg"]["type"] == "image/pjpeg"))
-		&& ($_FILES["smallimg"]["size"] < 20000))
-  {
-  if ($_FILES["smallimg"]["error"] > 0)
-    {
-    echo "Return Code: " . $_FILES["smallimg"]["error"] . "<br />";
-    }
-  else
-    {
-    echo "Upload: " . $_FILES["smallimg"]["name"] . "<br />";
-    echo "Type: " . $_FILES["smallimg"]["type"] . "<br />";
-    echo "Size: " . ($_FILES["smallimg"]["size"] / 1024) . " Kb<br />";
-    echo "Temp file: " . $_FILES["smallimg"]["tmp_name"] . "<br />";
-
-    if (file_exists("../images/impact/" . $_FILES["smallimg"]["name"]))
-      {
-      echo $_FILES["smallimg"]["name"] . " already exists. ";
-      }
-    else
-      {
-      move_uploaded_file($_FILES["smallimg"]["tmp_name"],
-      "../images/impact/" . $_FILES["smallimg"]["name"]);
-      echo "Stored in: " . "../images/impact/" . $_FILES["smallimg"]["name"];
-      }
-    }
   }
-else
-  {
-  echo "Invalid file";
-  }
-  //largeimg
-	if ((($_FILES["largeimg"]["type"] == "image/gif")
-		|| ($_FILES["largeimg"]["type"] == "image/jpeg")
-		|| ($_FILES["largeimg"]["type"] == "image/pjpeg"))
-		&& ($_FILES["largeimg"]["size"] < 20000))
-  {
-  if ($_FILES["largeimg"]["error"] > 0)
-    {
-    echo "Return Code: " . $_FILES["largeimg"]["error"] . "<br />";
-    }
-  else
-    {
-    echo "Upload: " . $_FILES["largeimg"]["name"] . "<br />";
-    echo "Type: " . $_FILES["largeimg"]["type"] . "<br />";
-    echo "Size: " . ($_FILES["largeimg"]["size"] / 1024) . " Kb<br />";
-    echo "Temp file: " . $_FILES["largeimg"]["tmp_name"] . "<br />";
-
-    if (file_exists("../images/impact/" . $_FILES["largeimg"]["name"]))
-      {
-      echo $_FILES["largeimg"]["name"] . " already exists. ";
-      }
-    else
-      {
-      move_uploaded_file($_FILES["largeimg"]["tmp_name"],
-      "../images/impact/" . $_FILES["largeimg"]["name"]);
-      echo "Stored in: " . "../images/impact/" . $_FILES["largeimg"]["name"];
-      }
-    }
-  }
-else
-  {
-  echo "Invalid file";
-  }
-
-  		$smallimgurl = $_FILES["smallimg"]["name"];
-		$largeimgurl = $_FILES["largeimg"]["name"];
-		if($id == Null) {
-			$q = "INSERT INTO impact VALUES (default,'$pic_desc','$small_desc','$description','$smallimgurl','$largeimgurl',default);";
-			$r = mysql_query($q);
-			if ($r == true)
-				echo "New IMPACT story created";
-			else
-				echo "New IMPACT story creation failed";
+  else {
+	$pic_desc = stripslashes($_POST['pic_desc']);
+	$small_desc = stripslashes($_POST['small_desc']);
+	$description = nl2br(stripslashes($_POST['description']));
+	if ((($_FILES["smallimg"]["type"] == "image/gif")
+		 || ($_FILES["smallimg"]["type"] == "image/jpeg")
+		 || ($_FILES["smallimg"]["type"] == "image/pjpeg"))
+		&& ($_FILES["smallimg"]["size"] < 20000)) {
+	  if ($_FILES["smallimg"]["error"] > 0) {
+		echo "Return Code: " . $_FILES["smallimg"]["error"] . "<br />";
+	  }
+	  else {
+		echo "Upload: " . $_FILES["smallimg"]["name"] . "<br />";
+		echo "Type: " . $_FILES["smallimg"]["type"] . "<br />";
+		echo "Size: " . ($_FILES["smallimg"]["size"] / 1024) . " Kb<br />";
+		echo "Temp file: " . $_FILES["smallimg"]["tmp_name"] . "<br />";
+		if (file_exists("../images/impact/" . $_FILES["smallimg"]["name"])) {
+		  echo $_FILES["smallimg"]["name"] . " already exists. ";
 		}
 		else {
-			$q = "UPDATE impact SET pic_desc='$pic_desc', small_desc='$small_desc', description='$description'";
-			if($smallimgurl != null)
-				$q .= ", smallimgurl='$smallimgurl'";
-			if($largeimgurl != null)
-				$q .= ", largeimgurl='$largeimgurl'";
-			$q .= " WHERE id=$id;";
-			$r = mysql_query($q);
-			if($r==true)
-				echo "Impact Story with id = " . $id . "updated";
-			else
-				echo "Updation Failed";
+		  move_uploaded_file($_FILES["smallimg"]["tmp_name"],
+							 "../images/impact/" . $_FILES["smallimg"]["name"]);
+		  echo "Stored in: " . "../images/impact/" . $_FILES["smallimg"]["name"];
 		}
-}
-echo $entry_display;
+	  }
+	}
+	else {
+	  $entry_display = "<br/>Missing/Invalid small image";
+	}
+		//largeimg
+	if ((($_FILES["largeimg"]["type"] == "image/gif")
+		 || ($_FILES["largeimg"]["type"] == "image/jpeg")
+		 || ($_FILES["largeimg"]["type"] == "image/pjpeg"))
+		&& ($_FILES["largeimg"]["size"] < 20000)) {
+	  if ($_FILES["largeimg"]["error"] > 0) {
+		echo "Return Code: " . $_FILES["largeimg"]["error"] . "<br />";
+	  }
+	  else {
+		echo "Upload: " . $_FILES["largeimg"]["name"] . "<br />";
+		echo "Type: " . $_FILES["largeimg"]["type"] . "<br />";
+		echo "Size: " . ($_FILES["largeimg"]["size"] / 1024) . " Kb<br />";
+		echo "Temp file: " . $_FILES["largeimg"]["tmp_name"] . "<br />";
+		if (file_exists("../images/impact/" . $_FILES["largeimg"]["name"])) {
+			echo $_FILES["largeimg"]["name"] . " already exists. ";
+		}
+		else {
+		  move_uploaded_file($_FILES["largeimg"]["tmp_name"],
+							 "../images/impact/" . $_FILES["largeimg"]["name"]);
+		  echo "Stored in: " . "../images/impact/" . $_FILES["largeimg"]["name"];
+		}
+	  }
+	}
+	else {
+		$entry_display.="<br/>Missing/Invalid large image.";
+	}
+	$smallimgurl = $_FILES["smallimg"]["name"];
+	$largeimgurl = $_FILES["largeimg"]["name"];
+	if($id == Null) {
+	  $q = "INSERT INTO impact VALUES (default,'$pic_desc','$small_desc','$description','$smallimgurl','$largeimgurl',default);";
+	  $r = mysql_query($q);
+	  if ($r == true)
+		echo "New IMPACT story created";
+	  else
+		echo "New IMPACT story creation failed";
+	}
+	else {
+	  $q = "UPDATE impact SET pic_desc='$pic_desc', small_desc='$small_desc', description='$description'";
+	  if($smallimgurl != null)
+		$q .= ", smallimgurl='$smallimgurl'";
+	  if($largeimgurl != null)
+		$q .= ", largeimgurl='$largeimgurl'";
+	  $q .= " WHERE id=$id;";
+	  $r = mysql_query($q);
+	  if($r==true)
+		echo "Impact Story with id = " . $id . "updated";
+	  else
+		echo "Updation Failed";
+	}
+  }
+  echo $entry_display;
 ?>
--- dynamic.php
+++ dynamic.php
@@ -1,115 +1,150 @@
 <div id="content_news" class="content_item">
-        	<h1><a href="news.php"><img src="images/news.jpg" /></a></h1>
+  <h1>
+	<a href="news.php">
+	  <img src="images/news.jpg" />
+	</a>
+  </h1>
 <?php
-if(!isset($news_count))
+  if(!isset($news_count))
 	$news_count=2;
-if(!isset($impact_count))
+  if(!isset($impact_count))
 	$impact_count=2;
-if(!isset($video_count))
-	$video_count=1;
-$q = "SELECT * FROM news ORDER BY tm DESC LIMIT $news_count";
-$r = mysql_query($q);
-$entry_display ="";
-$entry_display1="";
-$entry_display2="";
-
-if ( $r !== false && mysql_num_rows($r) > 0 ) {
+  if(!isset($video_count))
+    $video_count=1;
+  $q = "SELECT * FROM news ORDER BY tm DESC LIMIT $news_count";
+  $r = mysql_query($q);
+  $entry_display ="";
+  $entry_display1="";
+  $entry_display2="";
+  if ( $r !== false && mysql_num_rows($r) > 0 ) {
 	while ( $a = mysql_fetch_assoc($r) ) {
-		$heading = stripslashes($a['heading']);
-		$id = $a['id'];
-        $smallimg = $a['smallimgurl'];
-		if($smallimg=="" or $smallimg==null)
-			$smallimg="../newsdefault.png";
-		$entry_display .=<<<ENTRY_DISPLAY
-			
-			<br />
-            <a href="news_expand.php?id=$id">
-            <div class="news_item">
-			<div class="news_item_img"><img src="images/news/$smallimg " width="100%" /></div>
-			<div class="news_item_matter">$heading</div>
-            </div>
-            </a>
-            <div style="clear:both"></div>
-
-            
-        
-
+	  $heading = stripslashes($a['heading']);
+	  $id = $a['id'];
+      $smallimg = $a['smallimgurl'];
+	  if($smallimg=="" or $smallimg==null)
+		$smallimg="../newsdefault.png";
+	  $entry_display.=<<<ENTRY_DISPLAY
+	  <br />
+	  <a href="news_expand.php?id=$id">
+		<div class="news_item">
+		  <div class="news_item_img">
+			<img src="images/news/$smallimg " width="100%" />
+		  </div>
+		  <div class="news_item_matter">$heading</div>
+		</div>
+	  </a>
+	  <div style="clear:both"></div>
 ENTRY_DISPLAY;
 	}
-	}
-	echo $entry_display;
-	?>           <br />
-            <a href="news.php"><p align="right">More News ....</p></a>
-            
-        </div>
-        <div id="content_impact" class="content_item">
-        	<h1><a href="impact.php"><img src="images/impact.jpg" /></a></h1>
-            <br />
+  }
+  echo $entry_display;
+?>
+  <br />
+  <a href="news.php">
+	<p align="right">More News ....</p>
+  </a>
+</div>
+<div id="content_impact" class="content_item">
+  <h1>
+	<a href="impact.php">
+	  <img src="images/impact.jpg" />
+	</a>
+  </h1>
+  <br />
 <?php
-$q = "SELECT * FROM impact ORDER BY tm DESC LIMIT $impact_count";
-    $r = mysql_query($q);
-
-    if ( $r !== false && mysql_num_rows($r) > 0 ) {
-      while ( $a = mysql_fetch_assoc($r) ) {
-	    $pic_desc = stripslashes($a['pic_desc']);
-	    $small_desc = stripslashes($a['small_desc']);
-        $smallimg = $a['smallimgurl'];
-        $id = $a['id'];
-		$entry_display1 .=<<<ENTRY_DISPLAY1
+  $ids_available_db=mysql_query("select id from impact");
+  $ids_available=array();
+  while($row=mysql_fetch_assoc($ids_available_db))
+	$ids_available[]=$row['id'];
+  $max_index=count($ids_available);
+  $distinct_ids=array();
+  if($max_index<=$impact_count)
+	$distinct_ids=$ids_available;
+  else {
+	$count=0;
+	while($count<$impact_count) {
+	  $random_id=$ids_available[rand(0,$max_index-1)];
+	  if(!in_array($random_id,$distinct_ids)) {
+		$distinct_ids[]=$random_id;
+		++$count;
+	  }
+	}
+  }
+  $id_string="(";
+  $first_string=true;
+  foreach($distinct_ids as $id) {
+	  if(!$first_string) {
+		$id_string .= ',';
+	  }
+	  $first_string=false;
+	  $id_string .= $id;
+  }
+  $id_string.=')';
+  $q = "SELECT * FROM impact where id in $id_string ORDER BY tm DESC";
+  $r = mysql_query($q);
+  if ( $r !== false && mysql_num_rows($r) > 0 ) {
+    while ( $a = mysql_fetch_assoc($r) ) {
+	  $pic_desc = stripslashes($a['pic_desc']);
+	  $small_desc = stripslashes($a['small_desc']);
+      $smallimg = $a['smallimgurl'];
+      $id = $a['id'];
+	  $entry_display1 .=<<<ENTRY_DISPLAY1
 		<a href="impact_expand.php?id=$id">		
-            <div class="news_item">
-            	<div class="news_item_img"><img src="images/impact/$smallimg " width="100%" />
-					<small>$pic_desc</small>
-				</div>
-                <div class="news_item_matter">$small_desc</div>
-            </div>
+          <div class="news_item">
+            <div class="news_item_img"><img src="images/impact/$smallimg " width="100%" />
+			  <small>$pic_desc</small>
+			</div>
+            <div class="news_item_matter">$small_desc</div>
+          </div>
         </a>
-            <div style="clear:both"></div>
-
-            
-        
-
+        <div style="clear:both"></div>
 ENTRY_DISPLAY1;
 	}
-	}
-	echo $entry_display1;
-	?>
-			<br />
-            <a href="impact.php"><p align="right">More Stories ....</p></a>
-        </div>
-		<div id="content_video" class="content_item_last" class="center">
-			<h1>
-				<a href="video.php"><img src="images/video2.jpg" /></a>
-			</h1>
-			<br />
-
-<?php
-$q = "SELECT * FROM video ORDER BY tm DESC LIMIT $video_count";
-$r = mysql_query($q);
-
-    if ( $r !== false && mysql_num_rows($r) > 0 ) {
-      while ( $a = mysql_fetch_assoc($r) ) {
-	    $heading = stripslashes($a['heading']);
-	    $link = stripslashes($a['link']);
-        $smallimg = $a['smallimgurl'];
-		if($smallimg=="" or $smallimg==null)
-			$smallimg="../videodefault.png";
-		$entry_display2 .=<<<ENTRY_DISPLAY2
-			$heading	<br /><br />
-         <span class="center"> <a href="$link" rel="prettyPhoto" title=""><img src="images/video/$smallimg" align="absmiddle" /></a></span>
-		<br />
-		<br />
-		<a href="video.php"><p align="right">More Videos ....</p></a>
-
-      </div>
-        </div>
-    <div class="clear_all">&nbsp;</div>
- 
+  }
+  echo $entry_display1;
+?>
+  <br />
+  <a href="impact.php">
+	<p align="right">More Stories ....</p>
+  </a>
 </div>
-        
-
+<div id="content_video" class="content_item_last" class="center">
+  <h1>
+	<a href="video.php">
+	  <img src="images/video2.jpg" />
+	</a>
+  </h1>
+  <br />
+<?php
+  $q = "SELECT * FROM video ORDER BY tm DESC LIMIT $video_count";
+  $r = mysql_query($q);
+  if ( $r !== false && mysql_num_rows($r) > 0 ) {
+    while ( $a = mysql_fetch_assoc($r) ) {
+	  $heading = stripslashes($a['heading']);
+	  $link = stripslashes($a['link']);
+	  $smallimg = $a['smallimgurl'];
+	  if($smallimg=="" or $smallimg==null)
+		$smallimg="../videodefault.png";
+	  $entry_display2 .=<<<ENTRY_DISPLAY2
+			$heading
+		    <br />
+			<br />
+			<span class="center">
+			  <a href="$link" rel="prettyPhoto" title="">
+				<img src="images/video/$smallimg" align="absmiddle" />
+			  </a>
+			</span>
+			<br />
+			<br />
+			<a href="video.php">
+			  <p align="right">More Videos ....</p>
+			</a>
+          </div>
+        </div>
+        <div class="clear_all">&nbsp;</div>
+      </div>
 ENTRY_DISPLAY2;
 	}
-	}
-	echo $entry_display2;
-	?>
+  }
+  echo $entry_display2;
+?>
