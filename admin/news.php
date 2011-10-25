cd841a21589795454b0f0c98c0a6cbfcef5372db
Sandeep Ruhil <sandeep.kumar.ruhil@gmail.com>
2011-10-25 20:23:36 +0530

    Integrate news admin into a single file.
    	Proper navigation and messages for news upload/edit/delete.

--- admin/news.php
+++ admin/news.php
@@ -1,75 +1,196 @@
 <?php
-require_once('auth.php');
+  require_once('auth.php');
+  require_once('config.php');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>SS Admin | News</title>
-<link href="loginmodule.css" rel="stylesheet" type="text/css" />
-<script type="text/javascript">
-    function confirmDelete(){
+  <head>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<title>SS Admin | News</title>
+	<link href="loginmodule.css" rel="stylesheet" type="text/css" />
+	<script type="text/javascript">
+	  function confirmDelete(){
         return window.confirm('Are you sure you want to delete this?');
-    }
-</script>
-</head>
-<body>
-<h1> News Admin </h1>
-<?php
-	require_once('config.php');
-    $entry_display = <<<ADMIN_OPTION
-
+	  }
+	</script>
+  </head>
+  <body>
+	<h1> News Admin </h1>
 	<div class="admin_link" style="float:left">
-      <a href="edit_news.php">Add a New Entry</a>
+      <a href="?action=4">Add a New Entry</a>
     </div>
     <div style="float:right">
-        <a href="admin.php">Back to dashboard</a>
+      <a href="admin.php">Back to dashboard</a>
     </div>
     <div style="clear:both"></div>
-ADMIN_OPTION;
-
-    $q = "SELECT * FROM news ORDER BY tm DESC LIMIT 20";
-    $r = mysql_query($q);
-
-    if ( $r !== false && mysql_num_rows($r) > 0 ) {
-      while ( $a = mysql_fetch_assoc($r) ) {
-        $heading = stripslashes($a['heading']);
-        $description = nl2br(stripslashes($a['description']));
-		$id = stripslashes($a['id']);
-        $entry_display .= <<<ENTRY_DISPLAY
-
-    <h3>$heading</h3>
-    <p>
-      $description
-    </p>
-	<p><a href="edit_news.php?e=$id">Edit</a></p>
-	<p><a href="edit_news.php?e=$id&d=1" onclick="return confirmDelete();">Delete</a></p>
-	
-
-ENTRY_DISPLAY;
+	<?php
+	  /*
+	   * Actions to be performed for various values of $action
+	   * 0 => Show All the news.
+	   * 1 => Update the entry submitted.
+	   * 2 => Delete the entry.
+	   * 3 => Add a new entry.
+       * 4 => Show the form to get details for new entry.
+       * 5 => Show the form to edit an entry.
+	   */
+	  $action=0;
+      $message='';
+      $news_img_dir='../images/news/';
+      $allowed_file_types=array('image/gif','image/jpeg','image/pjpeg','image/png');
+      if(array_key_exists('action',$_GET)) {
+        $action=$_GET['action'];
       }
-    } else {
-      $entry_display = <<<ENTRY_DISPLAY
-
+      if(array_key_exists('action',$_POST)) {
+	    $action=stripslashes($_POST['action']);
+      }
+      if($action == 1) {
+		/* Update */
+        $heading = stripslashes($_POST['heading']);
+        $description = stripslashes($_POST['description']);
+        $id = $_POST['id'];
+        if(mysql_query("update news set heading='$heading',description='$description' where id = $id"))
+          $message .= 'The news was updated successfully.';
+      }
+      if($action == 2) {
+		  /* Deletion */
+		  $id=$_GET['id'];
+          $query = mysql_query("select * from news where id=$id");
+          $row=mysql_fetch_assoc($query);
+          if($row['smallimgurl'] !== null && $row['smallimgurl'] !== '' && file_exists($news_img_dir.$row['smallimgurl']))
+            unlink($news_img_dir.$row['smallimgurl']);
+          if($row['largeimgurl'] !== null && $row['largeimgurl'] !== '' && file_exists($news_img_dir.$row['largeimgurl']))
+            unlink($news_img_dir.$row['largeimgurl']);
+          if(mysql_query("delete from news where id=$id"))
+            $message .= 'The news was deleted successfully.';
+      }
+      if($action == 3) {
+		/* Addition */
+        $heading=stripslashes($_POST['heading']);
+        $description = stripslashes($_POST['description']);
+        $small_img_filename = '';
+        $large_img_filename = '';
+		if($_FILES['smallimg']['name'] !== '') {
+		  if($_FILES["smallimg"]["size"] > SMALL_IMG_FILE_SIZE) {
+		    $message .= 'The small image is larger than the maximum allowed size so was not uploaded.<br/>';
+	      } else {
+			$allowed=false;
+			foreach($allowed_file_types as $type)
+			  if($_FILES['smallimg']['type'] == $type)
+				$allowed=true;
+			if(!$allowed) {
+				$message .= 'The small image file is not a valid image so was not uploaded.<br/>';
+			} else {
+			  if(!file_exists($news_img_dir.$_FILES['smallimg']['name'])) {
+				$small_img_filename=$_FILES['smallimg']['name'];
+			  } else {
+				$fileparts=explode('.',$_FILES['smallimg']['name']);
+				$extension='.'.array_pop($fileparts);
+				$name=implode('.',$fileparts).'_';
+				$filecount=0;
+				while(file_exists($news_img_dir.$name.$filecount.$extension))
+				  $filecount=$filecount+1;
+				$small_img_filename=$name.$filecount.$extension;
+			  }
+			}
+			move_uploaded_file($_FILES['smallimg']['tmp_name'],$news_img_dir.$small_img_filename);
+		  }
+		}
+		if($_FILES['largeimg']['name'] !== '') {
+		  if($_FILES["largeimg"]["size"] > LARGE_IMG_FILE_SIZE) {
+		    $message .= 'The large image is larger than the maximum allowed size so was not uploaded.<br/>';
+	      } else {
+			$allowed=false;
+			foreach($allowed_file_types as $type)
+			  if($_FILES['largeimg']['type'] == $type)
+				$allowed=true;
+			if(!$allowed) {
+				$message .= 'The large image file is not a valid image so was not uploaded..<br/>';
+			} else {
+			  if(!file_exists($news_img_dir.$_FILES['largeimg']['name'])) {
+				$large_img_filename=$_FILES['largeimg']['name'];
+			  } else {
+				$fileparts=explode('.',$_FILES['largeimg']['name']);
+				$extension='.'.array_pop($fileparts);
+				$name=implode('.',$fileparts).'_';
+				$filecount=0;
+				while(file_exists($news_img_dir.$name.$filecount.$extension))
+				  $filecount=$filecount+1;
+				$large_img_filename=$name.$filecount.$extension;
+			  }
+			}
+			move_uploaded_file($_FILES['largeimg']['tmp_name'],$news_img_dir.$large_img_filename);
+		  }
+		}
+		if(mysql_query("INSERT INTO news VALUES (default,'$heading','$description','$small_img_filename','$large_img_filename',default);"))
+			$message .= 'New news created successfully.';
+      }
+      if($action == 4) {
+		/* Blank form */
+    ?>
+	<form action="news.php" method="post" enctype="multipart/form-data">
+	  <p><label for="id_heading">Heading:</label> <input id="id_heading" type="text" name="heading" /></p>
+	  <p><label for="id_description">Description:</label> <textarea name="description" id="id_description"></textarea></p>
+	  <p><label for="id_smallimg">Small Img File:</label> <input type="file" name="smallimg" id="id_smallimg" />Only jpg/gif images allowed, size &lt;2MB</p>
+      <p><label for="id_largeimg">Large Img File:</label> <input type="file" name="largeimg" id="id_largeimg" /></p>
+	  <input type="hidden" name="action" value="3" />
+	  <input type="submit" value="Upload" />
+	  <input type="button" value="Cancel" onclick="javascript:window.location='?';" />
+	</form>
+	<?php
+      }
+      if($action == 5) {
+		/* Editing form */
+        $id = $_GET['id'];
+        $result = mysql_query("select * from news where id=$id");
+        $row = mysql_fetch_assoc($result);
+    ?>
+	<form action="news.php" method="post" enctype="multipart/form-data">
+	  <p><label for="id_heading">Heading:</label> <input id="id_heading" type="text" name="heading" value="<?php echo $row['heading']; ?>" /></p>
+	  <p><label for="id_description">Description:</label> <textarea name="description" id="id_description"><?php echo $row['description']; ?></textarea></p>
+	  <p>Small Img File:<a href="<?php echo $news_img_dir,$row['smallimgurl']; ?>"><?php echo $row['smallimgurl']; ?></a></p>
+	  <p>Large Img File:<a href="<?php echo $news_img_dir,$row['largeimgurl']; ?>"><?php echo $row['largeimgurl']; ?></a></p>
+	  <input type="submit" value="Update" />
+	  <input type="button" value="Cancel" onclick="javascript:window.location='?';" />
+	  <input type="hidden" name="action" value="1"/>
+	  <input type="hidden" name="id" value="<?php echo $id; ?>"/>
+	</form>
+	<?php
+	  } /* End of actions */
+      // Do not show the news if an entry form was displayed
+      if($action != 4 && $action !=5) {
+        $q = "SELECT * FROM news ORDER BY tm DESC";
+        $r = mysql_query($q);
+        echo $message;
+        if ( $r == false || mysql_num_rows($r) == 0 ) {
+    ?>
     <h4>No news to display</h4>
     <p>
       No entries have been made on this page.
       Please check back soon, or click the
       link below to add an entry!
 	  <div class="admin_link" style="float:left">
-		<a href="edit_news.php">Add a New Entry</a>
+		<a href="?action=4">Add a New Entry</a>
       </div>
       <div style="float:right">
         <a href="admin.php">Back to dashboard</a>
       </div>
       <div style="clear:both"></div>
     </p>
-
-ENTRY_DISPLAY;
+	<?php
+        } else {
+          while ( $a = mysql_fetch_assoc($r) ) {
+            $id = stripslashes($a['id']);
+    ?>
+    <h3><?php echo stripslashes($a['heading']); ?></h3>
+    <p>
+      <?php echo nl2br(stripslashes($a['description'])); ?>
+    </p>
+	<p><a href="?action=5&id=<?php echo $id; ?>">Edit</a></p>
+	<p><a href="?action=2&id=<?php echo $id; ?>" onclick="return confirmDelete();">Delete</a></p>
+	<?php
+        }
+      }
     }
-
-    echo $entry_display;
-  
-?>
+    ?>
 </body>
 </html>
