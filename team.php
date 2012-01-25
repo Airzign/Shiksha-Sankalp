f500b4ab9c5561ffe64aa19d53192ce73cc08a88
Sandeep Ruhil <sandeep.kumar.ruhil@gmail.com>
2012-01-25 05:37:37 +0530

    Add team view and data to the database.

--- team.php
+++ team.php
@@ -1,128 +1,52 @@
 <?php
-include "headerinner.php";
-include "admin/config.php";
-?>
-     <script type="text/javascript" src="js/jquery.team.js"></script>
-  <div id="content">
-  		<div id="main_content">
-        	<!-- the content goes here -->
-<div class="innertitle">Team</div>
-
-<div class="innertext">
-Shiksha Sankalp has been founded by a group of professionals who are passionate about addressing difficulties in school education of children from poor families.
-</div>
-<br /><br/>
-<div class="innersubheading_main"><u>Board of Trustees of Shiksha Sankalp Foundation (India)</u></div>
-<br/>
-<div class="innersubheading_team">
-Mr. Shekhar Bhatia
-</div>
-<div class="innertext">
-high court lawyer, educationist and an IT businessman
-</div>
-<br/>
-<div class="innersubheading_team">
-Mr. Gaurav Malhotra
-</div>
-<div class="innertext">
-reputed architect
-</div>
- <br/>
-<div class="innersubheading_team">
-Mr. Ashok Sharma
-</div>
-<div class="innertext">
-<img src="images/Ashok Sharma.jpg" height="108" width="84" /><br />quality and industrial productivity consultant
-</div>
-<br/><br/>
-<div class="innersubheading_main"><u>
-Advisors to the Board</u>
-</div>
-<br/>
-<div class="innersubheading_team">
-Mr. R.K.Bhatia
-</div>
-<div class="innertext">
-<img src="images/RKBhatia.jpg" height="108" width="84" /> <br />Retired DDG in Department of Telecom, Government of India
-</div>
-<br/>
-<div class="innersubheading_team">
-Ms. Sapna Jain
-</div>
-<div class="innertext">
-<img src="images/Sapna Jain.jpg" height="108" width="84" />
-Senior Technical Director at AT&T (USA)
-</div>
- <br/>
- <br/>
-
-
-
-<div class="innersubheading_main"><u>Board of Directors of Shiksha Sankalp Incorporated (USA)</u></div>
-
-<br/>
-<div class="innersubheading_team">
-Mr. Mikul Bhatia
-</div>
-<div class="innertext">
-<img src="images/Mikul.jpg" height="108" width="84" />
-Senior Energy Specialist at the World Bank
-</div>
-<br/>
-<div class="innersubheading_team">
-Mr. Sagheer Ahmed
-</div>
-<div class="innertext">
-senior technical staff at Nvidia Corporation with eight US patents to his credit
-</div>
-<br/>
-<div class="innersubheading_team">
-Prof. Deborah Hughes Hallet
-</div>
-<div class="innertext">
-<img src="images/Deb Hughes Hallett.jpg" height="108" width="84" />
-senior faculty at Harvard and Arizona Universities
-</div>
-<br />
-<div class="innersubheading_team">
-Mr. James R. Breiding
-</div>
-<div class="innertext">
-<img src="images/Jeb Breiding.jpg" height="108" width="84" />
-founder and CEO of Naissance Capital
-</div>
-<br/>
-
-
-<div class="innersubheading_team">
-Ms. Baishali Mukherjee
-</div>
-<div class="innertext">
-<img src="images/Baishali.jpg" height="108" width="84" />
-a learning and development professional with HSBC and Sapient corporations
-</div>
-<br/>
-<div class="innersubheading_team">
-Mr.Suhail Kassim
-</div>
-<div class="innertext">
-Senior Private Sector Development Specialist at the World Bank
-</div>
-<br/>
-<div class="innersubheading_team">
-Ms. Kalpana Jain
-</div>
-<div class="innertext">
-senior Journalist at Business Standard
-</div>
-<br/>
-
-<!-- the content ends here -->
-
-        </div>
-        <div id="extra_content">
-		<?php
-		include "dynamic.php";
-		echo "</div>";
-		include "footer.php";
-		?>
+   include "headerinner.php";
+   include "admin/config.php";
+   ?>
+<div id="content">
+  <div id="main_content">
+    <!-- the content goes here -->
+	<div class="innertitle">The Team</div>
+	<p class="innertext">
+	  The business and affairs of Shiksha Sankalp Incorporated are governed by
+	  its Board of Directors and office bearers.
+	</p>
+	<?php
+	   $member_file_dir='images/team/';
+       $categories = array('Board of Directors of Shiksha Sankalp Incorporated, USA',
+                           'Office Bearers of Shiksha Sankalp USA',
+                           'Advisors',
+                           'Board of Directors of Shiksha Sankalp Foundation, India');
+	   $i=0;
+	   while($i<4) {
+	 ?>
+	 <div class="innersubheading"><?php echo $categories[$i]; ?></div>
+	 <?php
+	      $results = mysql_query("select * from team where category=$i order by position");
+	      ++$i;
+	      while($row = mysql_fetch_assoc($results)) {
+	 ?>
+	 <div>
+	   <?php if($row['picture']!== '' ) { ?>
+	   <img src="<?php echo $member_file_dir,$row['picture']; ?>" class="team_img"/>
+	   <?php } ?>
+	   <p class="innertext">
+		 <b><?php echo $row['name']; ?></b>
+		 -
+		 <?php echo $row['post']; ?>
+	   </p>
+	   <p class="innertext">
+		 <?php echo $row['description']; ?>
+	   </p>
+	 </div>
+	 <div style="clear:both;"></div>
+	 <?php
+		  }
+	   }
+	 ?>
+  </div>
+  <div id="extra_content">
+	<?php
+	   include "dynamic.php";
+       echo "</div>";
+	   include "footer.php";
+	   ?>
