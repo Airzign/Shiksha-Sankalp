baf2615412dd549448c492a0317c3754461a84a3
Sandeep Ruhil <sandeep.kumar.ruhil@gmail.com>
2012-02-26 02:27:52 +0530

    Indentation changes

--- about.php
+++ about.php
@@ -7,53 +7,53 @@
     <!-- the content goes here -->
 	<div class="innertitle">About Shiksha Sankalp</div>
 	<div style="padding-right:20px;">
-	<p class="innertext">
-	  "Shiksha Sankalp" in Sanskrit means "Resolve for Education".  As the name
-	  itself indicates, Shiksha Sankalp is an effort to ensure that every
-	  under-privileged child in the world achieves age-appropriate level of
-	  academic knowledge and skills.
-	</p>
-	<p class="innertext">
-	  At Shiksha Sankalp, we have developed an in-depth understanding of the
-	  complex barriers that prevent poor families in developing countries from
-	  obtaining effective school education for their children. We understand why
-	  nearly half of primary school students in India are unable to read a
-	  simple grade-2 text. And why two of every three children in India drop-out
-	  of school by middle grades. We find that the inability of poor students to
-	  learn has its roots much beyond the quality of education at school. It
-	  stems largely from the socio-economic condition of their families. School
-	  education is a losing proposition for poor families and it does not make
-	  economic sense for poor families to invest time, effort and money into the
-	  education of their children.
-	</p>
-	<p class="innertext">
-	  The challenge of universal and effective school education is important,
-	  complex and urgent. At Shiksha Sankalp we know what it would take to
-	  spread effective education to millions of poor children within a short
-	  period of time.
-	</p>
-	<p class="innertext">
-	  To meet the challenge of universal education in India,
-	  <i>Shiksha Sankalp Foundation (SSF)</i>
-	  has been set-up as a Trust. The foundation has applied for registration
-	  with Income Tax Department for exemption from taxes under sections 12A and
-	  80G of the Income Tax Act. Further, it is also applying for FCRA clearance
-	  for receiving donor funds from abroad.
-	</p>
-	<p class="innertext">
-	  In the USA,
-	  <i>Shiksha Sankalp Incorporated (SSI)</i>
-	  has been set-up as a nonprofit public benefit corporation registered with
-	  the office of Secretary of State of the state of California. It has been
-	  approved for exemption from tax in the United States under Section
-	  501(c)(3) of the Internal Revenue Code. The mandate of SSI is to promote
-	  school education in all developing countries, though initially it is
-	  focused on India where it coordinates efforts with SSF.
-	</p>
-	<p class="innertext">
-	  To learn more about the model developed by Shiksha Sankalp,
-	  <a href="learning_rewards_model.php">click here</a>.
-	</p>
+	  <p class="innertext">
+	    "Shiksha Sankalp" in Sanskrit means "Resolve for Education".  As the name
+	    itself indicates, Shiksha Sankalp is an effort to ensure that every
+	    under-privileged child in the world achieves age-appropriate level of
+	    academic knowledge and skills.
+	  </p>
+	  <p class="innertext">
+	    At Shiksha Sankalp, we have developed an in-depth understanding of the
+	    complex barriers that prevent poor families in developing countries from
+	    obtaining effective school education for their children. We understand why
+	    nearly half of primary school students in India are unable to read a
+	    simple grade-2 text. And why two of every three children in India drop-out
+	    of school by middle grades. We find that the inability of poor students to
+	    learn has its roots much beyond the quality of education at school. It
+	    stems largely from the socio-economic condition of their families. School
+	    education is a losing proposition for poor families and it does not make
+	    economic sense for poor families to invest time, effort and money into the
+	    education of their children.
+	  </p>
+	  <p class="innertext">
+	    The challenge of universal and effective school education is important,
+	    complex and urgent. At Shiksha Sankalp we know what it would take to
+	    spread effective education to millions of poor children within a short
+	    period of time.
+	  </p>
+	  <p class="innertext">
+	    To meet the challenge of universal education in India,
+	    <i>Shiksha Sankalp Foundation (SSF)</i>
+	    has been set-up as a Trust. The foundation has applied for registration
+	    with Income Tax Department for exemption from taxes under sections 12A and
+	    80G of the Income Tax Act. Further, it is also applying for FCRA clearance
+	    for receiving donor funds from abroad.
+	  </p>
+	  <p class="innertext">
+	    In the USA,
+	    <i>Shiksha Sankalp Incorporated (SSI)</i>
+	    has been set-up as a nonprofit public benefit corporation registered with
+	    the office of Secretary of State of the state of California. It has been
+	    approved for exemption from tax in the United States under Section
+	    501(c)(3) of the Internal Revenue Code. The mandate of SSI is to promote
+	    school education in all developing countries, though initially it is
+	    focused on India where it coordinates efforts with SSF.
+	  </p>
+	  <p class="innertext">
+	    To learn more about the model developed by Shiksha Sankalp,
+	    <a href="learning_rewards_model.php">click here</a>.
+	  </p>
 	</div>
   </div>
   <div id="extra_content">
