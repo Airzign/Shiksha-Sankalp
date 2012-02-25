84c3806ad47d990c41ae92bb2122b5ce0d735cf9
Sandeep Ruhil <sandeep.kumar.ruhil@gmail.com>
2012-02-26 02:47:52 +0530

    Solve H11

--- vision.php
+++ vision.php
@@ -4,52 +4,54 @@
    ?>
 <div id="content">
   <div id="main_content">
-	<!-- the content goes here -->
-	<div class="innertitle" style="font-size:50px;">Vision, Mission and Values</div>
-	<div class="innersubheading">Vision</div>
-	<p class="innertext">
-	  Shiksha Sankalp's vision is that every economically underprivileged child
-	  in the world attains school education by 2050.
-	</p>
-	<hr/>
-	<div class="innersubheading">Mission Statement</div>
-	<p class="innertext">
-	  To provide outcome based assistance to economically weak families based on
-	  demonstrated academic performance of their school-going children, by
-	  leveraging support from donors all over the world, based on trust
-	  emanating from high degree of transparency and accountability, with the
-	  ultimate aim of alleviating poverty and achieving universal education
-	  within the foreseeable future.
- 	</p>
-	<hr/>
-	<div class="innersubheading">Core Values</div>
-	<p class="innertext">
-	  Shiksha Sankalp's three core values are Trust, Transparency and
-	  Accountability, as defined below:
-	</p>
-	<p class="innertext">
-	  1.
-	  <b>Trust:</b>
-	  Shiksha Sankalp would strive to earn the trust of all stakeholders
-	  including beneficiaries, donors, volunteers, employees and the society
-	  at large, as a core value guiding all its policies and efforts.
-	</p>
-	<p class="innertext">
-	  2.
-	  <b>Transparency:</b>
-	  Shiksha Sankalp would adhere to highest standards of transparency by
-	  allowing access to all relevant information to stakeholders including
-	  beneficiaries, donors, volunteers, employees and society at large.
-	</p>
-	<p class="innertext">
-	  3.
-	  <b>Accountability:</b>
-	  Shiksha Sankalp would build accountability in all aspects of its
-	  functioning, including selection of project modules, verification of
-	  beneficiary eligibility, evaluation of learning achievement, monitoring
-	  of performance at school, and transfer of incentive amount from donors
-	  to beneficiaries.
- 	</p>
+    <!-- the content goes here -->
+    <div class="innertitle" style="font-size:50px;">Vision, Mission and Values</div>
+    <div style="padding-right:20px;">
+      <div class="innersubheading">Vision</div>
+      <p class="innertext">
+	Shiksha Sankalp's vision is that every economically underprivileged child
+	in the world attains school education by 2050.
+      </p>
+      <hr/>
+      <div class="innersubheading">Mission Statement</div>
+      <p class="innertext">
+	To provide outcome based assistance to economically weak families based on
+	demonstrated academic performance of their school-going children, by
+	leveraging support from donors all over the world, based on trust
+	emanating from high degree of transparency and accountability, with the
+	ultimate aim of alleviating poverty and achieving universal education
+	within the foreseeable future.
+      </p>
+      <hr/>
+      <div class="innersubheading">Core Values</div>
+      <p class="innertext">
+	Shiksha Sankalp's three core values are Trust, Transparency and
+	Accountability, as defined below:
+      </p>
+      <p class="innertext">
+	1.
+	<b>Trust:</b>
+	Shiksha Sankalp would strive to earn the trust of all stakeholders
+	including beneficiaries, donors, volunteers, employees and the society
+	at large, as a core value guiding all its policies and efforts.
+      </p>
+      <p class="innertext">
+	2.
+	<b>Transparency:</b>
+	Shiksha Sankalp would adhere to highest standards of transparency by
+	allowing access to all relevant information to stakeholders including
+	beneficiaries, donors, volunteers, employees and society at large.
+      </p>
+      <p class="innertext">
+	3.
+	<b>Accountability:</b>
+	Shiksha Sankalp would build accountability in all aspects of its
+	functioning, including selection of project modules, verification of
+	beneficiary eligibility, evaluation of learning achievement, monitoring
+	of performance at school, and transfer of incentive amount from donors
+	to beneficiaries.
+      </p>
+    </div>
   </div>
   <!-- the content ends here -->
   <div id="extra_content">
