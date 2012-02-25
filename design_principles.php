c7c0886b934b244f42ec53deab628a56595a5b2c
Sandeep Ruhil <sandeep.kumar.ruhil@gmail.com>
2012-02-26 02:35:42 +0530

    Solve H8

--- design_principles.php
+++ design_principles.php
@@ -5,57 +5,59 @@
 <div id="content">
   <div id="main_content">
     <!-- the content goes here -->
-	<div class="innertitle">DESIGN PRINCIPLES</div>
-	<p class="innertext">
-	  The fundamental principles underlying the design of the Shiksha Sankalp
-	  model are:
-	</p>
-	<p class="innertext">
-	  1.
-	  <b>Demand Side Incentives based on Demonstrated Outcomes:</b>
-	  Performance based demand-side incentives put responsibility on students
-	  and families to take education seriously and work towards better academic
-	  performance.
-	</p>
-	<p class="innertext">
-	  2.
-	  <b>Direct Cash Transfers to Eligible Families:</b>
-	  At Shiksha Sankalp, we believe direct cash transfers are the most
-	  economically efficient means to deliver donor support to eligible
-	  beneficiaries.
-	</p>
-	<p class="innertext">
-	  3.
-	  <b>Leveraging Markets for Provision of Education Services:</b>
-	  Shiksha Sankalp model encourages the development of market mechanisms for
-	  provision of educational services such as private schools, tuition
-	  centers, reading/arithmetic skill enhancement initiatives and peer-to-peer
-	  learning which complement the government school system to enhance learning
-	  outcomes.
-	</p>
-	<p class="innertext">
-	  4.
-	  <b>Sustainable, Scalable and Replicable Interventions:</b>
-	  Given the magnitude of education challenge in India and other developing
-	  countries, Shiksha Sankalp model has been designed to be sustainable,
-	  scalable and replicable.
-	</p>
-	<p class="innertext">
-	  5.
-	  <b>Transparency and Performance Accountability to Foster Trust:</b>
-	  The Learning Rewards model incorporates several checks and procedures to
-	  foster performance accountability. We adhere to highest standards of
-	  transparency by proactively disclosing all relevant information to
-	  respective stakeholders.
-	</p>
-	<p class="innertext">
-	  6.
-	  <b>Empowering Women for a Better Society:</b>
-	  Financial empowerment of women and enabling them to make educational
-	  decisions for their children has a far-reaching effect on the wider
-	  society. Also, higher performance incentives for girls provide them a
-	  greater encouragement to study.
-	</p>
+    <div class="innertitle">DESIGN PRINCIPLES</div>
+    <div style="padding-right:20px;">
+      <p class="innertext">
+	The fundamental principles underlying the design of the Shiksha Sankalp
+	model are:
+      </p>
+      <p class="innertext">
+	1.
+	<b>Demand Side Incentives based on Demonstrated Outcomes:</b>
+	Performance based demand-side incentives put responsibility on students
+	and families to take education seriously and work towards better academic
+	performance.
+      </p>
+      <p class="innertext">
+	2.
+	<b>Direct Cash Transfers to Eligible Families:</b>
+	At Shiksha Sankalp, we believe direct cash transfers are the most
+	economically efficient means to deliver donor support to eligible
+	beneficiaries.
+      </p>
+      <p class="innertext">
+	3.
+	<b>Leveraging Markets for Provision of Education Services:</b>
+	Shiksha Sankalp model encourages the development of market mechanisms for
+	provision of educational services such as private schools, tuition
+	centers, reading/arithmetic skill enhancement initiatives and peer-to-peer
+	learning which complement the government school system to enhance learning
+	outcomes.
+      </p>
+      <p class="innertext">
+	4.
+	<b>Sustainable, Scalable and Replicable Interventions:</b>
+	Given the magnitude of education challenge in India and other developing
+	countries, Shiksha Sankalp model has been designed to be sustainable,
+	scalable and replicable.
+      </p>
+      <p class="innertext">
+	5.
+	<b>Transparency and Performance Accountability to Foster Trust:</b>
+	The Learning Rewards model incorporates several checks and procedures to
+	foster performance accountability. We adhere to highest standards of
+	transparency by proactively disclosing all relevant information to
+	respective stakeholders.
+      </p>
+      <p class="innertext">
+	6.
+	<b>Empowering Women for a Better Society:</b>
+	Financial empowerment of women and enabling them to make educational
+	decisions for their children has a far-reaching effect on the wider
+	society. Also, higher performance incentives for girls provide them a
+	greater encouragement to study.
+      </p>
+    </div>
   </div>
   <div id="extra_content">
 	<?php
