b6fa7956d195e7f8b8d1af9010dd07aeefac9910
Sandeep Ruhil <sandeep.kumar.ruhil@gmail.com>
2012-02-26 02:39:34 +0530

    Solve H9

--- design_principles.php
+++ design_principles.php
@@ -13,21 +13,24 @@
       </p>
       <p class="innertext">
 	1.
-	<b>Demand Side Incentives based on Demonstrated Outcomes:</b>
+	<b>Demand Side Incentives based on Demonstrated Outcomes</b>
+	<br/>
 	Performance based demand-side incentives put responsibility on students
 	and families to take education seriously and work towards better academic
 	performance.
       </p>
       <p class="innertext">
 	2.
-	<b>Direct Cash Transfers to Eligible Families:</b>
+	<b>Direct Cash Transfers to Eligible Families</b>
+	<br/>
 	At Shiksha Sankalp, we believe direct cash transfers are the most
 	economically efficient means to deliver donor support to eligible
 	beneficiaries.
       </p>
       <p class="innertext">
 	3.
-	<b>Leveraging Markets for Provision of Education Services:</b>
+	<b>Leveraging Markets for Provision of Education Services</b>
+	<br/>
 	Shiksha Sankalp model encourages the development of market mechanisms for
 	provision of educational services such as private schools, tuition
 	centers, reading/arithmetic skill enhancement initiatives and peer-to-peer
@@ -36,14 +39,16 @@
       </p>
       <p class="innertext">
 	4.
-	<b>Sustainable, Scalable and Replicable Interventions:</b>
+	<b>Sustainable, Scalable and Replicable Interventions</b>
+	<br/>
 	Given the magnitude of education challenge in India and other developing
 	countries, Shiksha Sankalp model has been designed to be sustainable,
 	scalable and replicable.
       </p>
       <p class="innertext">
 	5.
-	<b>Transparency and Performance Accountability to Foster Trust:</b>
+	<b>Transparency and Performance Accountability to Foster Trust</b>
+	<br/>
 	The Learning Rewards model incorporates several checks and procedures to
 	foster performance accountability. We adhere to highest standards of
 	transparency by proactively disclosing all relevant information to
@@ -51,7 +56,8 @@
       </p>
       <p class="innertext">
 	6.
-	<b>Empowering Women for a Better Society:</b>
+	<b>Empowering Women for a Better Society</b>
+	<br/>
 	Financial empowerment of women and enabling them to make educational
 	decisions for their children has a far-reaching effect on the wider
 	society. Also, higher performance incentives for girls provide them a
