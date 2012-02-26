bffff07dde8f573bafd57c35023512f53653216f
Sandeep Ruhil <sandeep.kumar.ruhil@gmail.com>
2012-02-26 18:29:54 +0530

    admin : implement new menu

--- admin/menu.php
+++ admin/menu.php
@@ -1,44 +1,68 @@
 <div class="menu">
-  <div class="menuitem">
-	<a href="news.php">
-	  News Admin
-	</a>
-  </div>
-  <div class="menuitem">
-	<a href="impact.php">
-	  Impact Admin
-	</a>
-  </div>
-  <div class="menuitem">
-	<a href="video.php">
-	  Video Admin
-	</a>
-  </div>
-  <div class="menuitem">
-	<a href="documents.php">
-	  Documents Admin
-	</a>
-  </div>
-  <div class="menuitem">
-	<a href="events.php">
-	  Events Admin
-	</a>
-  </div>
-  <div class="menuitem">
-	<a href="board_meeting.php">
-	  Board Meetings
-	</a>
-  </div>
-  <div class="menuitem">
-	<a href="team.php">
-	  Team
-	</a>
-  </div>
-  <div class="menuitem">
-	<a href="logout.php">
-	  Logout
-	</a>
-  </div>
-  <div style="clear:both"></div>
+
+  <table cellspacing="0" style="margin-bottom:40px;">
+
+    <tr class="menu-row">
+      <td class="menu-title"><a href="logout.php" style="color:#4f81bd;">Logout</a></td>
+      <td class="menu-link"></td>
+      <td class="menu-link"></td>
+      <td class="menu-link"></td>
+      <td class="menu-link"></td>
+      <td class="menu-link"></td>
+      <td class="menu-link"></td>
+    </tr>
+
+    <tr class="menu-row">
+      <td class="menu-title">Updates</td>
+      <td class="menu-link"><a href="news.php">News</a></td>
+      <td class="menu-link"><a href="impact.php">Impact Stories</a></td>
+      <td class="menu-link"><a href="video.php">Videos</a></td>
+      <td class="menu-link"><a href="events.php">Events</a></td>
+      <td class="menu-link"></td>
+      <td class="menu-link"></td>
+    </tr>
+
+    <tr class="menu-row">
+      <td class="menu-title">About Us</td>
+      <td class="menu-link"><a href="documents.php">Documents</a></td>
+      <td class="menu-link"><a href="board_meeting.php">Board Meetings</a></td>
+      <td class="menu-link"><a href="team.php">Our Team</a></td>
+      <td class="menu-link"></td>
+      <td class="menu-link"></td>
+      <td class="menu-link"></td>
+    </tr>
+
+    <tr class="menu-row">
+      <td class="menu-title">Resources</td>
+      <td class="menu-link"><a href="research_papers.php">Research Papers</a></td>
+      <td class="menu-link"><a href="internet_links.php">Internet Links</a></td>
+      <td class="menu-link"><a href="communication_materials.php">Communication Materials</a></td>
+      <td class="menu-link"><a href="opetational_materials.php">Operational Materials</a></td>
+      <td class="menu-link"><a href="testing_materials.php">Testing Materials</a></td>
+      <td class="menu-link"><a href="newsletters.php">Newsletters</a></td>
+    </tr>
+
+    <tr class="menu-row">
+      <td class="menu-title">Accountability</td>
+      <td class="menu-link"><a href="summary_statistics.php">Summary Statistics</a></td>
+      <td class="menu-link"><a href="annual_reports.php">Annual Reports</a></td>
+      <td class="menu-link"><a href="tax_returns.php">Tax Returns</a></td>
+      <td class="menu-link"></td>
+      <td class="menu-link"></td>
+      <td class="menu-link"></td>
+    </tr>
+
+    <tr class="menu-row">
+      <td class="menu-title">Others</td>
+      <td class="menu-link"><a href="modules.php">Modules</a></td>
+      <td class="menu-link"><a href="faq.php">FAQs</a></td>
+      <td class="menu-link"><a href="newsletter_subscribers.php">Newsletter Subscribers</a></td>
+      <td class="menu-link"></td>
+      <td class="menu-link"></td>
+      <td class="menu-link"></td>
+    </tr>
+
+  </table>
+
 </div>
 
