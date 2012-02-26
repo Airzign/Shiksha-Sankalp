45933a600a6404a92cc1706a7ab7175a0ec8e71a
Sandeep Ruhil <sandeep.kumar.ruhil@gmail.com>
2012-02-26 17:10:41 +0530

    Admin template, page one finished.

--- admin/admin.php
+++ admin/admin.php
@@ -4,20 +4,44 @@ require_once('auth.php');
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Shiksha Sankalp Admin Dashboard</title>
-<link href="loginmodule.css" rel="stylesheet" type="text/css" />
-</head>
-<body>
-<h1> Shikhsa Sankalp Admin</h1>
-<ul>
-	<li><a href="news.php">News Admin</a></li>
-	<li><a href="impact.php">Impact Admin</a></li>
-	<li><a href="video.php">Video Admin</a></li>
-	<li><a href="documents.php">Documents Admin</a></li>
-	<li><a href="events.php">Events Admin</a></li>
-	<li><a href="logout.php">logout</a></li>
-</ul>
-</body>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <title>Shiksha Sankalp Admin Dashboard</title>
+    <link href="loginmodule.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <h1> Shikhsa Sankalp Admin</h1>
+
+    <p class="admin-menu-heading">Updates</p>
+    <p class="admin-menu-content"><a href="news.php">News</a></p>
+    <p class="admin-menu-content"><a href="impact.php">Impact Stories</a></p>
+    <p class="admin-menu-content"><a href="video.php">Videos</a></p>
+    <p class="admin-menu-content"><a href="events.php">Events</a></p>
+
+    <p class="admin-menu-heading">About Us</p>
+    <p class="admin-menu-content"><a href="documents.php">Legal Documents</a></p>
+    <p class="admin-menu-content"><a href="board_meeting.php">Board Meetings</a></p>
+    <p class="admin-menu-content"><a href="team.php">Our Team</a></p>
+
+    <p class="admin-menu-heading">Resources</p>
+    <p class="admin-menu-content"><a href=".php">Research Papers</a></p>
+    <p class="admin-menu-content"><a href=".php">Internet Links</a></p>
+    <p class="admin-menu-content"><a href=".php">Communication Materials</a></p>
+    <p class="admin-menu-content"><a href=".php">Operational Materials</a></p>
+    <p class="admin-menu-content"><a href=".php">Testing Materials</a></p>
+    <p class="admin-menu-content"><a href=".php">Newsletters</a></p>
+
+    <p class="admin-menu-heading">Accountability</p>
+    <p class="admin-menu-content"><a href=".php">Summary Statistics</a></p>
+    <p class="admin-menu-content"><a href=".php">Annual Reports</a></p>
+    <p class="admin-menu-content"><a href=".php">Tax Returns</a></p>
+
+    <p class="admin-menu-heading">Others</p>
+    <p class="admin-menu-content"><a href=".php">Modules</a></p>
+    <p class="admin-menu-content"><a href=".php">FAQs</a></p>
+    <p class="admin-menu-content"><a href=".php">Newsletter Subscribers</a></p>
+
+    <p class="admin-menu-heading"><a href="logout.php">Logout</a></p>
+
+  </body>
 </html>
