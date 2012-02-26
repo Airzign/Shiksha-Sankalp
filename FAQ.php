4d076c904ce8382ae804b0e7eb7214bd5f8a5513
Sandeep Ruhil <sandeep.kumar.ruhil@gmail.com>
2012-02-26 16:25:40 +0530

    Solve L2

--- FAQ.php
+++ FAQ.php
@@ -6,7 +6,7 @@
   <div id="content">
 	<div id="main_content">
 	    <div class="innertitle">Frequently Asked Questions</div>
-	    <h3 class="question">1. Why does Shiksha Sankalp incentivize students rather than opening more schools?</h3>
+	    <p class="question">1. Why does Shiksha Sankalp incentivize students rather than opening more schools?</p>
 	    <p class="answer">
 	      At Shiksha Sankalp we believe that poor educational outcomes are largely a result of demand side constraints
 	      and not supply side constraints. Poor families find it difficult to afford the overheads of educating their
@@ -14,7 +14,7 @@
 	      students while leveraging the existing network of schools.
 	    </p>
 
-		<h3 class="question">2. How does Shiksha Sankalp decide the incentive amount for a child?</h3>
+		<p class="question">2. How does Shiksha Sankalp decide the incentive amount for a child?</p>
 	    <p class="answer">
 	      The incentive amount for a child enrolled into the Shiksha Sankalp program is determined based
 	      on performance in the ASAP-test in accordance with a Performance-Incentive vector. The Performance Incentive
@@ -22,7 +22,7 @@
 		  to a student with his/her performance in the Annual Standardized Academic Performance (ASAP) Test.
 		</p>
 
-		<h3 class="question">3. Do performance incentives put psychological pressure on young minds?</h3>
+		<p class="question">3. Do performance incentives put psychological pressure on young minds?</p>
 		<p class="answer">
 		  Performance incentives put the responsibility on students and their families to take education seriously and
 		  work towards better academic performance.  Although they add to the psychological pressure on young minds, we
@@ -31,14 +31,14 @@
 		  meritocracy among students and their families.
 		</p>
 
-		<h3 class="question">4. Is the Shiksha Sankalp model scalable? </h3>
+		<p class="question">4. Is the Shiksha Sankalp model scalable? </p>
 		<p class="answer">
 		  This model is highly scalable as it is much less effort and management bandwidth intensive than opening and
 		  operating schools. Also, the Shiksha Sankalp model is structured into modules, and can be easily scaled-up by
 		  opening more and more modules.
 		</p>
 
-		<h3  class="question">5. Is the Shiksha Sankalp model cost effective? </h3>
+		<p  class="question">5. Is the Shiksha Sankalp model cost effective? </p>
 		<p class="answer">
 		  Shiksha Sankalp is a very cost effective model. It leverages existing schools infrastructure and other
 		  education opportunities available to students and families by incentivizing focus on improving academic
@@ -47,20 +47,20 @@
 	      administrative overheads and maximizes the delivery efficiency of donor contributions.
 	    </p>
 
-		<h3  class="question">6. How does Shiksha Sankalp lead to women’s empowerment? </h3>
+		<p  class="question">6. How does Shiksha Sankalp lead to women's empowerment? </p>
 	    <p class="answer">
 	      The Shiksha Sankalp model enhances the financial strength of the lady of the house by opening a bank account
 	      for her and transfering the monetary incentives into that very account
 	    </p>
 
-		<h3  class="question">7. How can you contribute to the Shiksha Sankalp initiative? </h3>
+		<p  class="question">7. How can you contribute to the Shiksha Sankalp initiative? </p>
 	    <p class="answer">
 	      You can contribute to the Shiksha Sankalp initiative as a donor, a volunteer, a member or as an employee.
 	      Of course, other stakeholders such as students, parents, teachers, school administrators and people working
 	      with other NGOs also contribute to the Shiksha Sankalp initiative in their respective roles.
 	    </p>
 
-		<h3  class="question">8. How does Shiksha Sankalp ensure complete transparency? </h3>
+		<p  class="question">8. How does Shiksha Sankalp ensure complete transparency? </p>
 	    <ul class="answer">
 	      <li>Verifiable Proofs from Independent Agencies such as election commission, school authorities, bank authorities.</li>
           <li>Cross-verification of all field information at the Shiksha Sankalp headquarters.</li>
@@ -71,38 +71,38 @@
 	      <li>Shiksha Sankalp shall disclose all statutory documents on its website.</li>
 	    </ul>
 
-		<h3  class="question">9. Has this approach been successfuly implemented somewhere else in the world?</h3>
+		<p  class="question">9. Has this approach been successfuly implemented somewhere else in the world?</p>
 		<p class="answer">
 		  The Shiksha Sankalp model falls into the category of solutions called as
 		  <em>Conditional Cash Transfers (CCTs)</em>
 		  , which have been implemented extensively in Mexico, Brazil and several other countries yielding positive results.
 		</p>
 
-		<h3  class="question">10. Does Shiksha Sankalp run schools or tuition centers?</h3>
+		<p  class="question">10. Does Shiksha Sankalp run schools or tuition centers?</p>
 	    <p class="answer">No. Shiksha Sankalp does not run schools or tuition centers. However, by providing
 		  performance based incentives, Shiksha Sankalp encourages poor families to tap into various paid and
 		  non-paid education service providers in the local area.
 		</p>
 
-		<h3  class="question">11. How can I make payments as a donor</h3>
+		<p  class="question">11. How can I make payments as a donor</p>
 	    <p class="answer">
 		  Payments may be made through the 'payment gateway'incorporated with the website.
 		  You may access the portal by clicking the 'Donate Now' portal on the home page.
 		</p>
 
-		<h3  class="question">12. What is the cost of sponsoring one child?</h3>
+		<p  class="question">12. What is the cost of sponsoring one child?</p>
 	    <p class="answer">The cost of sponsering one child is Rs 6000 per annum.</p>
 
-		<h3  class="question">13. How long is the period of sponsorship?</h3>
+		<p  class="question">13. How long is the period of sponsorship?</p>
 	    <p class="answer">The period of sponsership is one year. </p>
 
-		<h3  class="question">14. Can I sponsor a child for a period less than a year?</h3>
+		<p  class="question">14. Can I sponsor a child for a period less than a year?</p>
 	    <p class="answer">No, you cannot sponsor a child for less than a year. </p>
 
-		<h3  class="question">15. Are deductions made monthly or all at once?</h3>
+		<p  class="question">15. Are deductions made monthly or all at once?</p>
 	    <p class="answer">The entire amount for the whole year shall be deducted all at once from your account. </p>
 
-		<h3  class="question">16. How does shiksha sankalp use donor contributions?</h3>
+		<p  class="question">16. How does shiksha sankalp use donor contributions?</p>
 		<div class="answer">
 		  Shiksha Sankalp uses the donations to pay:
 		  <ul>
@@ -116,7 +116,7 @@
 		  a Balancing Account, which is separate for each donor.
 		</div>
 
-		<h3  class="question">17. What is the need to have a balancing account ?</h3>
+		<p  class="question">17. What is the need to have a balancing account ?</p>
 	    <p class="answer">
 		  The Balancing Account serves as a reserve for higher incentive payment in the future on account of (i) better
 		  student performance in the next ASAP-Test, (ii) annual inflation compensation, and (iii) higher incentive
@@ -124,13 +124,13 @@
 		  before an upward revision is necessitated.
 		</p>
 
-		<h3  class="question">18. Is it safe to give my credit card details online?</h3>
+		<p  class="question">18. Is it safe to give my credit card details online?</p>
 	  	<p class="answer">
 		  The payment gateway is being developed by a team of web developers and cyber security experts to ensure
 		  that your details are guarded by a foolproof method.
 		</p>
 
-		<h3  class="question">19. What shall I get after I make a donation?</h3>
+		<p  class="question">19. What shall I get after I make a donation?</p>
 	    <p class="answer">
 		  After you make a donation, you will be made available the periodic ASAP test (standardized performance assessment )
 		  results of the student that you have sponsered on your email  account. Further, Shiksha Sankalp shall
@@ -139,7 +139,7 @@
 		  parameters of the child they are sponsoring.
 		</p>
 
-		<h3  class="question">20. Will the donation help me in securing a tax break?</h3>
+		<p  class="question">20. Will the donation help me in securing a tax break?</p>
 	    <p class="answer">
 		  We are in the process of registering the organisation under section 80G of the Income Tax Act to obtain
 		  tax exemption for our donors.
