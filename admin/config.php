9eadd45d1209e68ab14551273eaf5d8ca3a29d57
Sandeep Ruhil <sandeep.kumar.ruhil@gmail.com>
2012-02-27 06:37:50 +0530

    admin : add communication materials admin

--- admin/config.php
+++ admin/config.php
@@ -6,6 +6,7 @@
     define('SMALL_IMG_FILE_SIZE',1048576);
     define('MEETING_FILE_SIZE',2097152);
     define('RESEARCH_PAPERS_FILE_SIZE',2097152);
+    define('COMMUNICATION_MATERIALS_FILE_SIZE',2097152);
     define('LARGE_IMG_FILE_SIZE',2097152);
     	//Connect to mysql server
 	$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
